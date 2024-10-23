d1191446bc7e4529e05f4d1d8f6a71558d549499
Paulina Wróbel <paulinawro13579@gmail.com>
2024-10-23 15:25:11 +0200

    ckeck True or False

--- palindromy-zadanie.py
+++ palindromy-zadanie.py
@@ -1,3 +1,10 @@
 def czy_palindrom(slowo):
     # Sprawdzenie, czy słowo jest równe swojemu odbiciu (odwrócona wersja)
     return slowo == slowo[::-1]
+
+slowo = "kanapka"
+
+if bool(czy_palindrom(slowo)) == True:
+    print(True)
+else:
+    print(False)
